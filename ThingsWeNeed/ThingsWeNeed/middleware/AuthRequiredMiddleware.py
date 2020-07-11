eab1b3675c513302ceadadd2f817e733a0f05da4
Krzysiek <krzysiek.ziach@gmail.com>
2020-07-11 14:05:12 +0200

    Removed authentication for now

--- ThingsWeNeed/ThingsWeNeed/middleware/AuthRequiredMiddleware.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import redirect
-
-class AuthRequiredMiddleware():
-    def process_request(self, request):
-        if not request.user.is_authenticated():
-            return redirect('login')
