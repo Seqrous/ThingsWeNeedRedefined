872b8e3cdfd1d3bb3248ceafb38ac8d37f652183
Krzysiek <krzysiek.ziach@gmail.com>
2020-07-20 16:36:50 +0200

    Update household_list display details and fix alert colour altering

--- ThingsWeNeed/ThingsWeNeed/settings.py
+++ ThingsWeNeed/ThingsWeNeed/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+from django.contrib.messages import constants as message_constants
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -72,6 +73,11 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'ThingsWeNeed.wsgi.application'
 
+MESSAGE_TAGS = {message_constants.DEBUG: 'debug',
+                message_constants.INFO: 'info',
+                message_constants.SUCCESS: 'success',
+                message_constants.WARNING: 'warning',
+                message_constants.ERROR: 'danger',}
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
