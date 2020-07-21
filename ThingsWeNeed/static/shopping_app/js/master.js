1e7db9cc76a03e5dfca5b0499b30de1643c6ac51
Krzysiek <krzysiek.ziach@gmail.com>
2020-07-21 11:22:11 +0200

    Added members collapsible with list displaying the members

--- ThingsWeNeed/static/shopping_app/js/master.js
+++ ThingsWeNeed/static/shopping_app/js/master.js
@@ -19,3 +19,19 @@ $(".remove-product-button").each(function(index) {
         form.attr('action', redirect)
     })
 })
+
+// COLLAPSIBLE
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
